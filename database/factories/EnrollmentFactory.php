dc07f5a7c40c491bebb02867c5181188965524d7
Ozair0101 <ozairjan186@gmail.com>
2025-11-02 12:20:20 +0430

    e

---
+++ database/factories/EnrollmentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Enrollment>
+ */
+class EnrollmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'student_id' => 1,
+            'grade_id' => 1,
+            'section_id' => 1,
+        ];
+    }
+}
