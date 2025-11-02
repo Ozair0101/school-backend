01805b4e2802f0c6929544d7a8f4b1fd497682db
Ozair0101 <ozairjan186@gmail.com>
2025-11-02 12:20:26 +0430

    e

---
+++ database/factories/ExamSubjectFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ExamSubject>
+ */
+class ExamSubjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'monthly_exam_id' => 1,
+            'subject_id' => 1,
+        ];
+    }
+}
